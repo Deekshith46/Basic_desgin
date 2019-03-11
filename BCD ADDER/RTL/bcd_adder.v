4040082d906925bd3fd70bbe0ecc63c8f791598a
Deekshith46 <deekshithhp01@gmail.com>
2019-03-11 13:11:15 +0530

    msg

--- BCD ADDER/RTL/bcd_adder.v
+++ BCD ADDER/RTL/bcd_adder.v
@@ -24,6 +24,88 @@ end
 
 endmodule
 
+ncvlog: *E,NOTSTT (verification.sv,148|11): expecting a statement [9(IEEE)].
+class agent extends uvm_component;
+                                |
+ncvlog: *E,SVNOTY (verification.sv,155|32): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,SVNOTY (verification.sv,157|30): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,SVEXTK (verification.sv,157|30): expecting a ';' (to terminate a type_declaration).
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,SVEXTK (verification.sv,157|30): expecting a ';' (to terminate a type_declaration).
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,SVNOTY (verification.sv,157|30): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,EXPSMC (verification.sv,157|30): expecting a semicolon (';') [10.3.1(IEEE)].
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,SVNOTY (verification.sv,157|30): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    `uvm_component_utils(agent)
+                              |
+ncvlog: *E,EXPSMC (verification.sv,157|30): expecting a semicolon (';') [10.3.1(IEEE)].
+(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 157)
+    function new (string path = "agent" , uvm_component parent = null);
+                                                      |
+ncvlog: *E,EXPRPA (verification.sv,159|54): expecting a right parenthesis (')') [A.2.6(IEEE)].
+    function new (string path = "agent" , uvm_component parent = null);
+                                                      |
+ncvlog: *E,NOIPRT (verification.sv,159|54): Unrecognized declaration 'uvm_component' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
+    function new (string path = "agent" , uvm_component parent = null);
+                                                      |
+ncvlog: *E,NOTINF (verification.sv,159|54): Interfaces are not allowed within tasks [SystemVerilog].
+    function new (string path = "agent" , uvm_component parent = null);
+                                                             |
+ncvlog: *E,ILLPDL (verification.sv,159|61): Mixing of ansi & non-ansi style port declaration is not legal.
+    function new (string path = "agent" , uvm_component parent = null);
+                                                               |
+ncvlog: *E,EXPSMC (verification.sv,159|63): expecting a semicolon (';') [12.3.2(IEEE)].
+     super.new(path,parent);
+         |
+ncvlog: *E,EXPRPA (verification.sv,160|9): expecting a right parenthesis (')') [A.2.6(IEEE)].
+    monitor m;
+          |
+ncvlog: *E,NOIPRT (verification.sv,163|10): Unrecognized declaration 'monitor' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
+    driver d;
+         |
+ncvlog: *E,NOIPRT (verification.sv,164|9): Unrecognized declaration 'driver' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
+    uvm_sequencer #(transaction) seqr;
+                |
+ncvlog: *E,EXPENC (verification.sv,165|16): Expecting the keyword 'endclass'.
+    virtual function void build_phase(uvm_phase phase);
+                   |
+ncvlog: *E,BADQAL (verification.sv,167|19): Lifetime or qualifier(s) 'virtual' not allowed before function declaration.
+    virtual function void build_phase(uvm_phase phase);
+                                              |
+ncvlog: *E,SVNOTY (verification.sv,167|46): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+    virtual function void connect_phase(uvm_phase phase);
+                   |
+ncvlog: *E,BADQAL (verification.sv,174|19): Lifetime or qualifier(s) 'virtual' not allowed before function declaration.
+    virtual function void connect_phase(uvm_phase phase);
+                                                |
+ncvlog: *E,SVNOTY (verification.sv,174|48): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
+endclass
+       |
+ncvlog: *E,MPANDC (verification.sv,179|7): expecting the keyword 'module', 'macromodule', 'primitive', 'connectmodule','connect', 'discipline' or 'nature' [A.1].
+	module worklib.add_tb:sv
+		errors: 0, warnings: 0
+	Total errors/warnings found outside modules and primitives:
+		errors: 23, warnings: 2
+irun: *E,VLGERR: An error occurred during parsing.  Review the log file for errors with the code *E and fix those identified problems to proceed.  Exiting with code (status 1).
+[vv2trainee2@compute-srv2 adder]$ 
+
+
 
 `timescale 1ns/1ps
 
@@ -54,17 +136,18 @@ class generator extends uvm_sequence #(transaction);
 
     //integer i;
 
-    function new(input string path = "generator" );
+    function new( string path = "generator" );
     super.new(path);
     endfunction
 
     virtual task body();
     transaction t;
-    t = transaction::type_id::create("t");
     repeat(10)
     begin
+    t = transaction::type_id::create("t");
     start_item(t);
-    t.randomize();
+    if(!t.randomize())
+    `uvm_error("GEN","Randomize failed");
     `uvm_info("GEN",$sformatf("Data send to driver a:%0d , b:%0d",t.a, t.b),UVM_NONE);
     finish_item(t);
     end
@@ -84,7 +167,7 @@ endinterface
 class driver extends uvm_driver#(transaction);
     `uvm_component_utils(driver)
 
-    function new (input string path = "driver" , uvm_component parent =  null);
+    function new ( string path = "driver" , uvm_component parent =  null);
      super.new(path,parent);
     endfunction
 
@@ -120,7 +203,7 @@ class monitor extends uvm_monitor;
 
     uvm_analysis_port #(transaction) send;
 
-    function new (input string path = "monitor" , uvm_component parent =  null);
+    function new (string path = "monitor" , uvm_component parent =  null);
      super.new(path,parent);
      send = new("send",this);
     endfunction
@@ -177,11 +260,12 @@ class scoreboard extends uvm_scoreboard;
 endclass
 
 ////////////agent///////////
-class agent extends uvm_agent;
+
+class agent extends uvm_component;
 
     `uvm_component_utils(agent)
 
-    function new (input string path = "agent" , uvm_component parent = null);
+    function new (string path = "agent" , uvm_component parent = null);
      super.new(path,parent);
     endfunction
 
@@ -208,8 +292,8 @@ class env extends uvm_env;
 
     `uvm_component_utils(env)
 
-    function new (input string path = "ENV" , uvm_component parent c);
-     super.new(inst,c);
+    function new (input string path = "env" , uvm_component parent = null);
+     super.new(path, parent);
     endfunction
 
     scoreboard s;
@@ -234,8 +318,8 @@ class test extends uvm_test;
 
     `uvm_component_utils(test)
 
-    function new (string path = "TEST" , uvm_component parent c);
-     super.new(inst,c);
+    function new (string path = "test" , uvm_component parent = null);
+     super.new(path , parent);
     endfunction
 
    generator gen;
@@ -271,95 +355,4 @@ uvm_config_db #(virtual add_if)::set(null,"uvm_test_top.e.a*","aif",aif);
 run_test("test");
 end
 endmodule
-
-|
-ncvlog: *W,DLCPTH (/tools/cadence/FOUNDRY/cds.lib,19): cds.lib Invalid path '/home/install/FOUNDRY/rfLib_new' (cds.lib command ignored).
-file: verification.sv
-    t.randomize();
-              |
-ncvlog: *W,FUNTSK (verification.sv,40|14): function called as a task without void'().
-        else
-           |
-ncvlog: *E,NOTSTT (verification.sv,147|11): expecting a statement [9(IEEE)].
-class agent extends uvm_agent;
-                            |
-ncvlog: *E,SVNOTY (verification.sv,153|28): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,SVNOTY (verification.sv,155|31): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,SVEXTK (verification.sv,155|31): expecting a ';' (to terminate a type_declaration).
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,SVEXTK (verification.sv,155|31): expecting a ';' (to terminate a type_declaration).
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,SVNOTY (verification.sv,155|31): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,EXPSMC (verification.sv,155|31): expecting a semicolon (';') [10.3.1(IEEE)].
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,SVNOTY (verification.sv,155|31): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                               |
-ncvlog: *E,EXPSMC (verification.sv,155|31): expecting a semicolon (';') [10.3.1(IEEE)].
-(`define macro: m_uvm_component_registry_internal [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 467], `define macro: uvm_component_utils [/tools/cadence/INCISIVE152/tools/methodology/UVM/CDNS-1.1d/sv/src/macros/uvm_object_defines.svh line 330], file: verification.sv line 155)
-    `uvm_component_utils (agent);
-                                |
-ncvlog: *W,UEXPSC (verification.sv,155|32): Ignored unexpected semicolon following SystemVerilog description keyword (endfunction).
-    function new (input string path = "agent" , uvm_component parent = null);
-                                                            |
-ncvlog: *E,EXPRPA (verification.sv,157|60): expecting a right parenthesis (')') [A.2.6(IEEE)].
-    function new (input string path = "agent" , uvm_component parent = null);
-                                                            |
-ncvlog: *E,NOIPRT (verification.sv,157|60): Unrecognized declaration 'uvm_component' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
-    function new (input string path = "agent" , uvm_component parent = null);
-                                                            |
-ncvlog: *E,NOTINF (verification.sv,157|60): Interfaces are not allowed within tasks [SystemVerilog].
-    function new (input string path = "agent" , uvm_component parent = null);
-                                                                   |
-ncvlog: *E,ILLPDL (verification.sv,157|67): Mixing of ansi & non-ansi style port declaration is not legal.
-    function new (input string path = "agent" , uvm_component parent = null);
-                                                                     |
-ncvlog: *E,EXPSMC (verification.sv,157|69): expecting a semicolon (';') [12.3.2(IEEE)].
-     super.new(path,parent);
-         |
-ncvlog: *E,EXPRPA (verification.sv,158|9): expecting a right parenthesis (')') [A.2.6(IEEE)].
-    monitor m;
-          |
-ncvlog: *E,NOIPRT (verification.sv,161|10): Unrecognized declaration 'monitor' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
-    driver d;
-         |
-ncvlog: *E,NOIPRT (verification.sv,162|9): Unrecognized declaration 'driver' could be an unsupported keyword, a spelling mistake or missing instance port list '()' [SystemVerilog].
-    uvm_sequencer #(transaction) seqr;
-                |
-ncvlog: *E,EXPENC (verification.sv,163|16): Expecting the keyword 'endclass'.
-    virtual function void build_phase(uvm_phase phase);
-                   |
-ncvlog: *E,BADQAL (verification.sv,165|19): Lifetime or qualifier(s) 'virtual' not allowed before function declaration.
-    virtual function void build_phase(uvm_phase phase);
-                                              |
-ncvlog: *E,SVNOTY (verification.sv,165|46): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-    virtual function void connect_phase(uvm_phase phase);
-                   |
-ncvlog: *E,BADQAL (verification.sv,172|19): Lifetime or qualifier(s) 'virtual' not allowed before function declaration.
-    virtual function void connect_phase(uvm_phase phase);
-                                                |
-ncvlog: *E,SVNOTY (verification.sv,172|48): Syntactically this identifier appears to begin a datatype but it does not refer to a visible datatype in the current scope.
-endclass
-       |
-ncvlog: *E,MPANDC (verification.sv,177|7): expecting the keyword 'module', 'macromodule', 'primitive', 'connectmodule','connect', 'discipline' or 'nature' [A.1].
-	module worklib.add_tb:sv
-		errors: 0, warnings: 0
-	Total errors/warnings found outside modules and primitives:
-		errors: 23, warnings: 4
-irun: *E,VLGERR: An error occurred during parsing.  Review the log file for errors with the code *E and fix those identified problems to proceed.  Exiting with code (status 1).
 
